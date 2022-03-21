b82589125bfc2ff4309df5766f5f46b20f2f69ff
DEEEDEEDEDEDEDEWEEEDEEDEDEDQEDQEFQEFQEFASDFVEQGWEHAEFBEA#%RHqe2t%rjhuaertjhnaertghiaetghametirhmaetrihaetrihmaetheKajajajajaja j aa I am depressed <luxter77@users.noreply.github.com>
2022-03-21 13:40:47 -0300

    It grabs headders now

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,10 @@ use std::thread;
 
 
 const CONNECTION_TIME: u64  = 2;
-const PORTS: &'static [u16] = &[21, 1021];
-const CORES: usize = 10;
+const PORTS: &'static [u16] = &[21];
+const CORES: usize = 20;
+const QUESTIONMARK: &'static str = "?";
+const NET_BUFFER: usize = 4096;
 
 fn load_work_into_queue(ip_queue: Arc<Mutex<Vec<Ipv4AddrRange>>>) -> () {
     
@@ -21,35 +23,53 @@ fn load_work_into_queue(ip_queue: Arc<Mutex<Vec<Ipv4AddrRange>>>) -> () {
     }
 }
 
-fn query_socket(ip: Ipv4Addr, port: u16) -> bool {
-    return TcpStream::connect_timeout(&SocketAddr::new(IpAddr::V4(ip), port), Duration::new(CONNECTION_TIME, 0)).is_ok();
+fn query_socket(ip: Ipv4Addr, port: u16, r: &mut [u8]) -> bool {
+    let stream = TcpStream::connect_timeout(&SocketAddr::new(IpAddr::V4(ip), port), Duration::new(CONNECTION_TIME, 0));
+    
+    if !stream.is_ok() {
+        return false
+    } else {
+        let resoult = std::io::Read::read(&mut stream.unwrap(), r);
+        if resoult.is_ok() {
+            return true;
+        } else {
+            return false;
+        };
+    };
 }
 
-fn proc_range(input: Arc<Mutex<Vec<Ipv4AddrRange>>>, output: Arc<Mutex<Vec<(Ipv4Addr, u16)>>>) -> () {
+fn proc_range(input: Arc<Mutex<Vec<Ipv4AddrRange>>>, output: Arc<Mutex<Vec<(Ipv4Addr, u16, [u8; NET_BUFFER])>>>) -> () {
     loop {
         let range = input.lock().unwrap().pop();
         if range.is_none() { break };
         for ip in range.unwrap() {
             for port in PORTS {
                 // print!("{:?}:{:?}... ", ip.clone(), port.clone());
-                if query_socket(ip.clone(), port.clone()) {
-                    output.lock().unwrap().push((ip.clone(), port.clone())); // println!("HIT!") 
+                let mut r: [u8; NET_BUFFER] = [0; NET_BUFFER];
+                if query_socket(ip.clone(), port.clone(), &mut r) {
+                    output.lock().unwrap().push((ip.clone(), port.clone(), r)); // println!("HIT!") 
                 } // else    { println!("boo") }
             }
         }
     }
 }
 
-fn format_record(record: (Ipv4Addr, u16), out: &mut [String; 2]) -> () {
+fn format_record(record: (Ipv4Addr, u16, [u8; NET_BUFFER]), out: &mut [String; 3]) -> () {
     let octets = record.0.octets();
     out[0] = format!("{}.{}.{}.{}", octets[0], octets[1], octets[2], octets[3]);
     out[1] = format!("{}", record.1);
+    out[2] = format!("{}", 
+        match std::str::from_utf8(&record.2) {
+            Ok(v) => v,
+            Err(_) => QUESTIONMARK.clone(),
+        }.trim_matches(char::from(0))
+    );
 }
 
 fn main() {
     let mut writer = csv::Writer::from_path("./extra/scannout.csv").expect("Can't write out file.");
     
-    let write_queue:    Arc<Mutex<Vec<(Ipv4Addr, u16)>>>    = Arc::new(Mutex::new(Vec::new()));
+    let write_queue:    Arc<Mutex<Vec<(Ipv4Addr, u16, [u8; NET_BUFFER])>>>    = Arc::new(Mutex::new(Vec::new()));
     let ip_queue:       Arc<Mutex<Vec<Ipv4AddrRange>>>      = Arc::new(Mutex::new(Vec::new()));
     
     load_work_into_queue(ip_queue.clone());
@@ -67,12 +87,12 @@ fn main() {
                 sleep(Duration::new(10*CONNECTION_TIME, 0));
             }
         } else {
-            let mut formatted_record: [String; 2] = [String::with_capacity(15), String::with_capacity(6)];
+            let mut formatted_record: [String; 3] = [String::with_capacity(15), String::with_capacity(6), String::with_capacity(NET_BUFFER)];
             let last = write_queue.lock().unwrap().pop().unwrap();
             format_record(last.clone(),  &mut formatted_record);
-            writer.write_record(formatted_record).unwrap();
+            writer.write_record(formatted_record.clone()).unwrap();
             writer.flush().unwrap();
-            println!("{:?}", last);
+            println!("{:?}", formatted_record);
         }
     }
 }
